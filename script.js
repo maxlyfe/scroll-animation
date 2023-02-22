a9c0916b81ab6e5040998295ad1c567b36bc1721
MaxLyFe <maximilianolyfbuzios@gmail.com>
2023-02-21 21:13:16 -0300

    JavaScript coment

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+/* Seleccionando todos los elementos con la clase de caja. */
 const boxes = document.querySelectorAll('.box');
 
+/* Escuchando el evento de desplazamiento y luego llamando a la función checkBoxes. */
 window.addEventListener('scroll', checkBoxes);
 
+/* Llamando a la función. */
 checkBoxes();
 
+/* Una función que se llama cuando se desplaza la página. */
 function checkBoxes() {
+    /* Está calculando la parte inferior de la ventana gráfica. */
     const triggerBottom = window.innerHeight / 5 * 4
 
     boxes.forEach( box => {
+        /* Cálculo de la parte superior de la caja. */
         const boxTop = box.getBoundingClientRect().top
 
+        /* Comprobando si la parte superior de la caja es menor que la parte inferior de la ventana. */
         if (boxTop < triggerBottom) {
             box.classList.add('show')
         } else {
